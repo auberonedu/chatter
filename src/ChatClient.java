22c040e4bbc7b7c16031f41a7443e3c029732179
Auberon López <auberon.lopez@greenriver.edu>
2025-11-20 09:51:24 -0800

    file version of livecode

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class ChatClient {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter the name of the file to read");
+        String filename = userInput.nextLine();
+
+        FileInputStream fileDataStream = new FileInputStream(filename);
+        InputStreamReader streamReader = new InputStreamReader(fileDataStream);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
