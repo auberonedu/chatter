6ba68dad179bfe92a0b2370819fc677285928153
Auberon López <auberon.lopez@greenriver.edu>
2025-12-02 23:07:03 +0000

    Day 2

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -22,16 +22,19 @@ public class ChatClient {
     }
     
     public static void readFromSocket(String host, int port) throws IOException {
-        Socket socket = new Socket(host, port);
 
-        InputStream inputStream = socket.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        
+        try (Socket socket = new Socket(host, port)) {
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        String line;
-        while((line = bufferedReader.readLine()) != null) {
-            System.out.println(line);
-        }
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } 
+
     }
 
     public static void readFromFile(String filename) throws IOException{
